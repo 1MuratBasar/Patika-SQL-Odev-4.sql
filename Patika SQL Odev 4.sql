febed3ad7a0925b53df47c95a9df42e8d9c582cb
Hawk1dot6 <107918661+Hawk1dot6@users.noreply.github.com>
2022-06-22 15:25:04 +0300

    Create Patika SQL Odev 4.sql

---
+++ Patika SQL Odev 4.sql
@@ -0,0 +1,35 @@
+--film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+
+select
+
+DISTINCT replacement_cost
+    
+from FILM;
+
+
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+
+select
+
+COUNT (DISTINCT replacement_cost)
+    
+from FILM;
+
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+
+select
+count (title) 
+from FILM
+where
+title LIKE ('T%')
+and
+rating = 'G'
+
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+
+select
+COUNT (DISTINCT country)
+from COUNTRY
+where
+
+country LIKE '_____'
